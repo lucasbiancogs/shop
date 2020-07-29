e36228b6ecbe91c5741a54e021f7c3258d008876
lucasbiancogs <lucas.biancogs@hotmail.com>
2020-07-29 07:50:38 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# shop
+
+A new Flutter project.
+
+## A simple E-commerce, developed with the flutter course from cod3r.
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'views/produtcts_overview_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Minha Loja',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ProductOverviewScreen(),
+    );
+  }
+}
