632e7939635d5cf779df0c02a45bb2ba1934df3e
lucasbiancogs <lucas.biancogs@hotmail.com>
2020-08-09 23:39:34 -0300

    Implementing form submitting

--- lib/views/product_form_screen.dart
+++ lib/views/product_form_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+
+import '../providers/product.dart';
 
 class ProductFormScreen extends StatefulWidget {
   @override
@@ -10,6 +14,15 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
   final _descriptionFocusNode = FocusNode();
   final _imageURLFocusNode = FocusNode();
   final _imageURLController = TextEditingController();
+  /*
+  É criada uma GlobalKey para vincular o estado do formulário com
+  uma função, visto que não temos controllers
+
+  Assim, ao chamar a função _saveForm, será pego o estado atual do Form
+  e nele aplicado o .save() função que chama o onSaved
+  */
+  final _form = GlobalKey<FormState>();
+  final _formData = Map<String, Object>();
 
   @override
   void initState() {
@@ -32,6 +45,20 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
     setState(() {});
   }
 
+  void _saveForm() {
+    _form.currentState.save();
+    final newProduct = Product(
+      id: Random().nextDouble().toString(),
+      title: _formData['title'],
+      price: _formData['price'],
+      description: _formData['description'],
+      imageUrl: _formData['imageUrl'],
+    );
+    print(newProduct.id);
+    print(newProduct.price);
+    print(newProduct.title);
+  }
+
   @override
   void dispose() {
     super.dispose();
@@ -46,10 +73,17 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Adicionar Produto'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.check),
+            onPressed: _saveForm,
+          )
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(15),
         child: Form(
+          key: _form,
           child: ListView(
             children: <Widget>[
               TextFormField(
@@ -63,6 +97,7 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
                   */
                   FocusScope.of(context).requestFocus(_priceFocusNode);
                 },
+                onSaved: (value) => _formData['title'] = value,
               ),
               TextFormField(
                 decoration: InputDecoration(labelText: 'Preço'),
@@ -72,12 +107,14 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_descriptionFocusNode);
                 },
+                onSaved: (value) => _formData['price'] = double.parse(value),
               ),
               TextFormField(
                 decoration: InputDecoration(labelText: 'Descrição'),
                 maxLines: 3,
                 keyboardType: TextInputType.multiline,
                 focusNode: _descriptionFocusNode,
+                onSaved: (value) => _formData['description'] = value,
               ),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
@@ -89,31 +126,34 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
                       textInputAction: TextInputAction.done,
                       focusNode: _imageURLFocusNode,
                       controller: _imageURLController,
+                      onFieldSubmitted: (_) {
+                        _saveForm();
+                      },
+                      onSaved: (value) => _formData['imageUrl'] = value,
                     ),
                   ),
                   Container(
-                    height: 100,
-                    width: 100,
-                    margin: EdgeInsets.only(
-                      top: 8,
-                      left: 10,
-                    ),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.grey,
-                        width: 1,
+                      height: 100,
+                      width: 100,
+                      margin: EdgeInsets.only(
+                        top: 8,
+                        left: 10,
                       ),
-                    ),
-                    alignment: Alignment.center,
-                    child: _imageURLController.text.isEmpty
-                        ? Text('Informe a URL')
-                        : FittedBox(
-                            child: Image.network(
-                              _imageURLController.text,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                  ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.grey,
+                          width: 1,
+                        ),
+                      ),
+                      alignment: Alignment.center,
+                      child: _imageURLController.text.isEmpty
+                          ? Text('Informe a URL')
+                          : FittedBox(
+                              child: Image.network(
+                                _imageURLController.text,
+                                fit: BoxFit.cover,
+                              ),
+                            )),
                 ],
               ),
             ],
