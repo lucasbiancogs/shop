4cf5e086406a32dde5d2c1677728ee19c0191f44
lucasbiancogs <lucas.biancogs@hotmail.com>
2020-08-19 02:16:20 -0300

    Implementing auto login with Shared Preferences

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,8 +1,10 @@
+import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:shop/exceptions/auth_exception.dart';
 
+import '../data/store.dart';
+import '../exceptions/auth_exception.dart';
 import '../utils/constants.dart';
 
 class Auth with ChangeNotifier {
@@ -12,6 +14,7 @@ class Auth with ChangeNotifier {
   String _token;
   String _userId;
   DateTime _expiryDate;
+  Timer _logoutTimer;
 
   bool get isAuth {
     return token != null;
@@ -39,7 +42,6 @@ class Auth with ChangeNotifier {
     );
 
     final responseBody = json.decode(response.body);
-
     if (responseBody['error'] != null) {
       throw AuthException(responseBody['error']['message']);
     } else {
@@ -48,6 +50,14 @@ class Auth with ChangeNotifier {
       _expiryDate = DateTime.now().add(
         Duration(seconds: int.parse(responseBody['expiresIn'])),
       );
+
+      Store.saveMap('userData', {
+        'token': _token,
+        'userId': _userId,
+        'expiryDate': _expiryDate.toIso8601String(),
+      });
+
+      _autoLogout();
       notifyListeners();
     }
 
@@ -60,5 +70,53 @@ class Auth with ChangeNotifier {
 
   Future<void> login(String email, String password) async {
     return _authenticate(email, password, _signinUrl);
+  }
+
+  Future<void> tryAutoLogin() async {
+    // Vai verificar se está logado
+    if (isAuth) {
+      return Future.value();
+    }
+
+    // Vai verificar se já tem usuário persistido
+    final userData = await Store.getMap('userData');
+    if (userData == null) {
+      return Future.value();
+    }
+
+    // Vai verificar se está dentro do prazo de expiração
+    final expiryDate = DateTime.parse(userData['expiryDate']);
+    if (expiryDate.isBefore(DateTime.now())) {
+      return Future.value();
+    }
+
+    _userId = userData['userId'];
+    _token = userData['token'];
+    _expiryDate = expiryDate;
+
+    _autoLogout();
+    notifyListeners();
+    return Future.value();
+  }
+
+  void logout() {
+    _token = null;
+    _userId = null;
+    _expiryDate = null;
+    if (_logoutTimer != null) {
+      _logoutTimer.cancel();
+      _logoutTimer = null;
+    }
+
+    Store.remove('userData');
+    notifyListeners();
+  }
+
+  void _autoLogout() {
+    if (_logoutTimer != null) {
+      _logoutTimer.cancel();
+    }
+    final timeToLogout = _expiryDate.difference(DateTime.now()).inSeconds;
+    _logoutTimer = Timer(Duration(seconds: timeToLogout), logout);
   }
 }
--- lib/views/auth_or_home.dart
+++ lib/views/auth_or_home.dart
@@ -10,8 +10,22 @@ class AuthOrHome extends StatelessWidget {
   Widget build(BuildContext context) {
     Auth auth = Provider.of<Auth>(context);
 
-    return auth.isAuth
-    ? ProductOverviewScreen()
-    : AuthScreen();
+    return FutureBuilder(
+      future: auth.tryAutoLogin(),
+      builder: (ctx, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(
+              valueColor:
+                  AlwaysStoppedAnimation(Theme.of(context).primaryColor),
+            ),
+          );
+        } else if (snapshot.error != null) {
+          return Center(child: Text('Ocorreu um erro na aplicação'));
+        } else {
+          return auth.isAuth ? ProductOverviewScreen() : AuthScreen();
+        }
+      },
+    );
   }
 }
